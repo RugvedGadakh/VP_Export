8d99ccb52d3b9861292d88f34c01bdeedfbbd197
RugvedGadakh <rugvedgadakh12@gmail.com>
2024-03-18 20:13:30 +0530

    Services added

---
+++ src/components/Services/Service.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./About.css"
+
+const Service = () => {
+    return (
+        <>
+            <section>
+                
+            </section>
+
+
+        </>
+    )
+}
+
+export default Service;
